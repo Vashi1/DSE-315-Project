cf948c8fab8bdce3266729a8b145b6d5d8bfbc99
Rakshith Vaishnavi Dogra <rakshithdogra@gmail.com>
2023-11-08 15:07:05 +0530

    Grid Search Updated

--- GridSearch_params.py
+++ GridSearch_params.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.naive_bayes import CategoricalNB
-
+from sklearn.ensemble import GradientBoostingClassifier
 
 def split_data(data_red, labels):
     X_Train, X_Test, y_train, y_test = train_test_split(data_red, labels, test_size=0.2, random_state=42)
@@ -60,13 +60,13 @@ def grid_search_lr(X_train, y_train):
     param_grid_lr = [
         {
             'penalty': [None, 'l2'],
-            'C': [1, 1.1, 1.2, 1.3, 1.4, 1.5, 2.0],
-            'solver': ['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga'],
+            'C': [1, 1.1],
+            'solver': ['lbfgs', 'liblinear', 'newton-cg', 'sag'],
         }
     ]
     gs_lr = GridSearchCV(lr, param_grid_lr, scoring='accuracy', cv=5, n_jobs=-1)
     gs_lr.fit(X_train, y_train)
-    print("Optimal Random Forest Tree Parameters: \n\n")
+    print("Optimal Logistic Regreesion Parameters: \n\n")
     print(gs_lr.best_params_)
     score = cross_val_score(gs_lr, X_train, y_train, scoring='accuracy', cv=5)
     print("Average Accuracy Score: %.4f +/- %.4f" % ((np.mean(score)), np.std(score)))
@@ -76,8 +76,8 @@ def grid_search_svm(X_train, y_train):
     svc = SVC(random_state=42)
     param_grid_svc = [
         {
-            'C': [1, 1.2, 1.3, 1.4, 1.5, 2.0],
-            'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+            'C': [1, 1.2,],
+            'kernel': ['linear', 'poly', 'sigmoid'],
             'gamma': ['scale', 'auto'],
             'shrinking': [True, False],
             'decision_function_shape': ['ovo', 'ovr']
@@ -85,7 +85,7 @@ def grid_search_svm(X_train, y_train):
     ]
     gr_svm = GridSearchCV(svc, param_grid_svc, scoring='accuracy', cv=5, n_jobs=-1)
     gr_svm.fit(X_train, y_train)
-    print("Optimal Random Forest Tree Parameters: \n\n")
+    print("Optimal Support Vector Machine Parameters: \n\n")
     print(gr_svm.best_params_)
     score = cross_val_score(gr_svm, X_train, y_train, scoring='accuracy', cv=5)
     print("Average Accuracy Score: %.4f +/- %.4f" % ((np.mean(score)), np.std(score)))
@@ -95,13 +95,28 @@ def grid_search_nb(X_train, y_train):
     nb = CategoricalNB()
     param_grid_nb = [
         {
-            'alpha': [1, 1.1, 1.2, 1.3, 1.5, 1.7, 2.0, 2.5],
+            'alpha': [1, 1.1, 2.5],
             'fit_prior': [True, False],
         }
     ]
     gr_nb = GridSearchCV(nb, param_grid_nb, cv=5, scoring='accuracy')
     gr_nb.fit(X_train, y_train)
-    print("Optimal Random Forest Tree Parameters: \n\n")
+    print("Optimal Naive Bayes Classifier Parameters: \n\n")
+    print(gr_nb.best_params_)
+    score = cross_val_score(gr_nb, X_train, y_train, scoring='accuracy', cv=5)
+    print("Average Accuracy Score: %.4f +/- %.4f" % ((np.mean(score)), np.std(score)))
+
+def grid_search_gb(X_train, y_train):
+    gb = GradientBoostingClassifier(random_state=42)
+    param_grid_gb = [
+        {
+            'loss' : ['log_loss', 'exponential'],
+            'learning_rate' : [0.1, 0.01, 0.02]
+        }
+    ]
+    gr_nb = GridSearchCV(gb, param_grid_gb, cv=5, scoring='accuracy')
+    gr_nb.fit(X_train, y_train)
+    print("Optimal Gradient Boosting Parameters: \n\n")
     print(gr_nb.best_params_)
     score = cross_val_score(gr_nb, X_train, y_train, scoring='accuracy', cv=5)
     print("Average Accuracy Score: %.4f +/- %.4f" % ((np.mean(score)), np.std(score)))
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Best Parameters Grid Search CV:
+{'ccp_alpha': 0.01, 'criterion': 'gini', 'max_depth': None, 'max_features': None, 'splitter': 'best'}
